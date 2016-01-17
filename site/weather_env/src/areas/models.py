46d8db211d71af43c4b54b3cc443394b56a7f8f0
Priti Wright <priti@energysavvy.com>
2016-01-17 11:35:55 -0800

    Created new Weather Django project

---
+++ site/weather_env/src/areas/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# class Area(models.Model):
+#     area_name = models.CharField(max_length=200)
+#     latitude_int = models.IntegerField()
+#     longitude_int = models.IntegerField()
+#     pub_date = models.DateTimeField('date published')
+#
+# class Timespan(models.Model):
+#     length = models.TimeField
